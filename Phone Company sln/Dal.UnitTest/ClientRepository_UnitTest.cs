8c204343249bf7253baa8669ca9788d50612a25c
nitzan selwyn <nitzansel@gmail.com>
2018-12-03 11:10:33 +0200

    Start To create client UI

--- Phone Company sln/Dal.UnitTest/ClientRepository_UnitTest.cs
+++ Phone Company sln/Dal.UnitTest/ClientRepository_UnitTest.cs
@@ -1,38 +1,115 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Common.Interfaces;
+using Common.Models;
+using Dal.DataInitializer;
+using Dal.DataModels;
+using Dal.ModelConverters;
+using Dal.Repositories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
 
 namespace Dal.UnitTest
 {
     [TestClass]
     public class ClientRepository_UnitTest
     {
+        private Mock<DbSet<DbClient>> _mockSet;
+        private Mock<PhoneCompanyContext> _mockContext;
+
         [TestMethod]
         public void AddNewClient_RecivedClient_UserWasAddedToTheDB()
         {
-            //Arrange
-            throw new NotImplementedException();
+            _mockSet = new Mock<DbSet<DbClient>>();
 
-            //Act
+            _mockContext = new Mock<PhoneCompanyContext>();
+            _mockContext.Setup(m => m.Clients).Returns(_mockSet.Object);
 
-            //Assert
+            var service = new ClientRepository(_mockContext.Object);
+            service.AddNewClient(ModelsGenerateForUnitTests.GenrateClient());
+
+            _mockSet.Verify(m => m.Add(It.IsAny<DbClient>()), Times.Once);
+            _mockContext.Verify(m => m.SaveChanges(), Times.Once);
         }
 
         [TestMethod]
         public void UpdateClient_NewIfoOnTheUserWasRecived_UserUpdatedInTheDB()
         {
-            //Arrange
-            throw new NotImplementedException();
+            var ClientData = new List<Client>
+            {
+                new Client
+                {
+                    Adress = "sadasd",CallToCenter = 1,ClientTypeId = 1,ContactNumber = "4561465",
+                    Id = 1,LastName = "asdasd",Name = "asdasd",SignDate = DateTime.Now,UserId = 1
+                },
+                new Client
+                {
+                    Adress = "sadasd",CallToCenter = 1212,ClientTypeId = 2,ContactNumber = "121212",
+                    Id = 2,LastName = "asdasd",Name = "asdasd",SignDate = DateTime.Now,UserId = 1
+                },
+                new Client
+                {
+                    Adress = "sadasd",CallToCenter = 1,ClientTypeId = 1,ContactNumber = "323232",
+                    Id = 3,LastName = "asdasd",Name = "asdasd",SignDate = DateTime.Now,UserId = 2
+                }
+            };
 
-            //Act
+            var repository = new Mock<IClientRepository>();
 
-            //Assert
+            //var context = new PhoneCompanyContext
+            //{
+            //    Clients = new Mock<DbSet<DbClient>>().Object
+            //};
+
+            //var clientToChange = new Client
+            //{
+            //    Id = 1,
+            //    Adress = "Hello, 12",
+            //    CallToCenter = 11,
+            //    ClientTypeId = 1,
+            //    ContactNumber = "0584313648",
+            //    LastName = "adsdasd",
+            //    Name = "sadasd",
+            //    SignDate = DateTime.Today,
+            //    UserId = 1
+            //};
+
+            //_mockSet = new Mock<DbSet<DbClient>>();
+
+            //_mockContext = new Mock<PhoneCompanyContext>();
+
+            //context.Clients = ModelsGenerateForUnitTests.GetQueryableMockDbSet(clientToChange.CommonToDb());
+
+            ////var clientToChange = context.Clients
+            ////    .Select(c => c.DbToCommon()).FirstOrDefault();
+
+            //_mockContext.Setup(m => m.Clients).Returns(_mockSet.Object);
+
+            //var service = new ClientRepository(_mockContext.Object);
+
+            //service.UpdateClient(new Client
+            //{
+            //    Id = 1,
+            //    Adress = "Hello, 12",
+            //    CallToCenter = 11,
+            //    ClientTypeId = 1,
+            //    ContactNumber = "0584313648",
+            //    LastName = "aaaaaa",
+            //    Name = "sadasd",
+            //    SignDate = DateTime.Today,
+            //    UserId = 1
+            //});
+
+            //Assert.AreEqual(clientToChange.LastName == "aaaaaa", true);
         }
 
         [TestMethod]
         public void DeleteClient_RecivedClientID_TheClientWillDeletedAndAddedToTheUnSignTable()
         {
             throw new NotImplementedException();
-
         }
 
         [TestMethod]
