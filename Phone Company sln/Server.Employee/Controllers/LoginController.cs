82e08f65cd0c46cb6fbc682adf2e51e501f314ea
nitzan selwyn <nitzansel@gmail.com>
2018-12-04 16:02:39 +0200

    almost finished login in employee UI

---
+++ Phone Company sln/Server.Employee/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using BL.Services;
+using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Server.Employee.Controllers
+{
+    public class LoginController : ApiController
+    {
+        private LoginService loginService = new LoginService();
+
+        [HttpPost]
+        public User UserLogin()
+        {
+            return loginService.Login()
+        }
+    }
+}
