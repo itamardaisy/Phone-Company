34606359e25b9c1ddd22569a8d844c79faa008c5
nitzan selwyn <nitzansel@gmail.com>
2018-12-06 21:41:33 +0200

    MostConnectedClientViewModel is done & MostProfitableClientView Model is done

--- Phone Company sln/UI.Employee/ViewModel/ManagerViewModel/MostConnectedClientViewModel.cs
+++ Phone Company sln/UI.Employee/ViewModel/ManagerViewModel/MostConnectedClientViewModel.cs
@@ -2,9 +2,14 @@
 using GalaSoft.MvvmLight.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using UI.Employee.Models;
+using Windows.UI.Popups;
 
 namespace UI.Employee.ViewModel
 {
@@ -13,15 +18,47 @@ namespace UI.Employee.ViewModel
         private readonly INavigationService _navigationService;
         public RelayCommand NavigateCommandToManagerPage { get; private set; }
 
+        public ObservableCollection<Client> Clients { get; set; }
+        private const string BASE_ADDRESS = "http://localhost:50066/api/employee/";
+        private HttpClient client;
+
         public MostConnectedClientViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
             NavigateCommandToManagerPage = new RelayCommand(GoBackCommand);
+
+            client = new HttpClient();
+            client.BaseAddress = new Uri(BASE_ADDRESS);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            GetClients();
         }
 
         private void GoBackCommand()
         {
             _navigationService.NavigateTo(pageKey: "ManagerMainPage");
+        }
+
+        private async void GetClients()
+        {
+            var myUri = new Uri(BASE_ADDRESS + "GetMostConnectedClient", UriKind.Absolute);
+
+            var message = await client.GetAsync(myUri);
+
+            using (HttpResponseMessage respone = message)
+            {
+                if (respone.IsSuccessStatusCode)
+                {
+                    var answer = await respone.Content.ReadAsAsync<ObservableCollection<Client>>();
+                    if (answer != null)
+                    {
+                        Clients = answer;
+                    }
+
+                }
+                await new MessageDialog("Bad Connection To The Server").ShowAsync();
+            }
         }
     }
 }
