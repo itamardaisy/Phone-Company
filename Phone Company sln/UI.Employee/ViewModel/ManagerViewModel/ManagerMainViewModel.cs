1d01c8852f9774c686d95ec5b46c06a798049ec9
nitzan selwyn <nitzansel@gmail.com>
2018-12-06 15:57:29 +0200

    navigation in manager pages is done and hte basic view model is working

--- Phone Company sln/UI.Employee/ViewModel/ManagerViewModel/ManagerMainViewModel.cs
+++ Phone Company sln/UI.Employee/ViewModel/ManagerViewModel/ManagerMainViewModel.cs
@@ -12,11 +12,39 @@ namespace UI.Employee.ViewModel
     {
         public INavigationService _navigationService { get; set; }
         public RelayCommand NavigateCommandToLogout { get; private set; }
+        public RelayCommand NavigateCommandToAnnoyingCustomer { get; private set; }
+        public RelayCommand NavigateCommandToAddNewPackage { get; private set; }
+        public RelayCommand NavigateCommandToMostConnectedClient { get; private set; }
+        public RelayCommand NavigateCommandToMostProfitableClient { get; private set; }
 
         public ManagerMainViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
             NavigateCommandToLogout = new RelayCommand(CommandToLogout);
+            NavigateCommandToAnnoyingCustomer = new RelayCommand(CommandToAnnoyingCustomer);
+            NavigateCommandToAddNewPackage = new RelayCommand(CommandToAddNewPackage);
+            NavigateCommandToMostConnectedClient = new RelayCommand(CommandToMostConnectedClient);
+            NavigateCommandToMostProfitableClient = new RelayCommand(CommandToMostProfitableClient);
+        }
+
+        private void CommandToMostProfitableClient()
+        {
+            _navigationService.NavigateTo("MostProfitableClientPage");
+        }
+
+        private void CommandToMostConnectedClient()
+        {
+            _navigationService.NavigateTo("MostConnectedClientPage");
+        }
+
+        private void CommandToAddNewPackage()
+        {
+            _navigationService.NavigateTo("AddNewPackagePage");
+        }
+
+        private void CommandToAnnoyingCustomer()
+        {
+            _navigationService.NavigateTo("MostAnnoyingCustomerPage");
         }
 
         private void CommandToLogout()
--- Phone Company sln/UI.Employee/ViewModel/ManagerViewModel/MostConnectedClientViewModel.cs
+++ Phone Company sln/UI.Employee/ViewModel/ManagerViewModel/MostConnectedClientViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,18 @@ namespace UI.Employee.ViewModel
 {
     internal class MostConnectedClientViewModel
     {
+        private readonly INavigationService _navigationService;
+        public RelayCommand NavigateCommandToManagerPage { get; private set; }
+
+        public MostConnectedClientViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            NavigateCommandToManagerPage = new RelayCommand(GoBackCommand);
+        }
+
+        private void GoBackCommand()
+        {
+            _navigationService.NavigateTo(pageKey: "ManagerMainPage");
+        }
     }
 }
